6b43014198cd72cb1ac0469d9dfd54831ff00df9
Jānis <97699850+janisslsm@users.noreply.github.com>
2023-10-26 20:01:22 +0300

    refactor: use findInReactTree

--- plugins/HideCallButtons/src/index.ts
+++ plugins/HideCallButtons/src/index.ts
@@ -1,35 +1,25 @@
 import { findByName } from "@vendetta/metro"
 import { after } from "@vendetta/patcher"
+import { getAssetIDByName } from "@vendetta/ui/assets";
+import { findInReactTree } from "@vendetta/utils";
 let patches = []
 
 export default {
     onLoad: () => {
-        const UserProfileHeader = findByName("UserProfileHeader", false);
         const UserProfileActions = findByName("UserProfileActions", false);
-        
-        patches.push(after("default", UserProfileHeader, (_, component) => {
-            const { props } = component;
-            const { children } = props
-            if(children === undefined) return;
-            const buttons = children[4]?.props?.children;
-            if(buttons === undefined) return;
-        
-            delete buttons[1];
-        
-            return [component]
-        }));
-        
+
         patches.push(after("default", UserProfileActions, (_, component) => {
             const { props } = component;
             const { children } = props
             if(children === undefined) return;
-            const buttons = children?.props?.children[1]?.props?.children;
+            const buttons = findInReactTree(component, (x) => x?.props.children[1].props.icon == getAssetIDByName("ic_video"))?.props.children
             if(buttons === undefined) return;
-        
+            delete buttons[0];
             delete buttons[1];
-        
+
             return [component]
         }));
+
     },
     onUnload: () => {
         for (const unpatch of patches) unpatch()

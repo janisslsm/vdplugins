bd6c0e3d3e71f6fdf3867490226d08fc65fb5870
Jānis <97699850+janisslsm@users.noreply.github.com>
2023-10-27 16:01:56 +0300

    feat(HideCallButtons): Hide DM call buttons

--- plugins/HideCallButtons/src/index.ts
+++ plugins/HideCallButtons/src/index.ts
@@ -2,17 +2,21 @@ import { findByName } from "@vendetta/metro"
 import { after } from "@vendetta/patcher"
 import { getAssetIDByName } from "@vendetta/ui/assets";
 import { findInReactTree } from "@vendetta/utils";
+import { storage } from "@vendetta/plugin";
+import Settings from "./settings";
 let patches = []
 
 export default {
     onLoad: () => {
+        storage.hideUserProfile ??= true;
+        storage.hideDMTitlebar ??= false;
+
         const UserProfileActions = findByName("UserProfileActions", false);
         const videoCallAsset = getAssetIDByName("ic_video");
-        
+        const Header = findByName("Header", false);
+
         patches.push(after("default", UserProfileActions, (_, component) => {
-            const { props } = component;
-            const { children } = props
-            if(children === undefined) return;
+            if(!storage.hideUserProfile) return [component];
             const buttons = findInReactTree(component, (x) => x?.props?.children[1]?.props?.icon == videoCallAsset)?.props?.children;
             if(buttons === undefined) return;
 
@@ -22,8 +26,31 @@ export default {
             return [component]
         }));
 
+        patches.push(after("default", Header, (_, component) => {
+            if(!storage.hideDMTitlebar) return;
+            
+            const _default = findInReactTree(component, (x) => x?.type?.name == "_default")
+            if(_default === undefined) return;
+            
+            after("type", _default, (_, channelnavbuttons) => {
+                if(channelnavbuttons?.type === undefined) return;
+
+                after("type", channelnavbuttons, (_, nonnullchannelnavbuttons) => {
+                    if(nonnullchannelnavbuttons?.type === undefined) return;
+
+                    after("type", nonnullchannelnavbuttons, (_, comp) => {
+                        delete comp?.props?.buttons[0];
+                        delete comp?.props?.buttons[1];
+                        return [comp];
+                    })
+                })
+            })
+
+            return [component]
+        }));
     },
     onUnload: () => {
         for (const unpatch of patches) unpatch()
-    }
+    },
+    settings: Settings
 }
---
+++ plugins/HideCallButtons/src/settings.tsx
@@ -0,0 +1,33 @@
+import { ReactNative } from "@vendetta/metro/common";
+import { Forms } from "@vendetta/ui/components";
+import { getAssetIDByName } from "@vendetta/ui/assets";
+import { storage } from "@vendetta/plugin";
+import { useProxy } from "@vendetta/storage";
+
+const { FormDivider, FormIcon, FormSwitchRow } = Forms;
+
+export default () => {
+  useProxy(storage);
+
+  return (
+    <ReactNative.ScrollView>
+      <FormSwitchRow
+        label="Hide from user profile"
+        subLabel="Hide call buttons from user profile"
+        leading={<FormIcon source={getAssetIDByName("ic_profile")} />}
+        onValueChange={(v) => {
+          storage.hideUserProfile = v;
+        }}
+        value={storage.hideUserProfile}
+      />
+      <FormDivider />
+      <FormSwitchRow
+        label="Hide from DM"
+        subLabel="Hide call buttons from DM title bar"
+        leading={<FormIcon source={getAssetIDByName("ic_dm")} />}
+        onValueChange={(v) => (storage.hideDMTitlebar = v)}
+        value={storage.hideDMTitlebar}
+      />
+    </ReactNative.ScrollView>
+  );
+};

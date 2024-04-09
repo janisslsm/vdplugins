e706f4fa683a637102d58404b0ee92b90ad1b7a4
Jānis <97699850+janisslsm@users.noreply.github.com>
2024-04-09 04:14:42 +0300

    fix(FakeNitro): implement option to toggle hyperlinks

--- plugins/FakeNitro/src/utils.ts
+++ plugins/FakeNitro/src/utils.ts
@@ -8,40 +8,43 @@ const { getGuildId } = findByStoreName("SelectedGuildStore");
 const hasEmotesRegex = /<a?:(\w+):(\d+)>/i;
 
 function extractUnusableEmojis(messageString: string, size: number) {
-	const emojiStrings = messageString.matchAll(/<a?:(\w+):(\d+)>/gi);
-	const emojiUrls = [];
+    const emojiStrings = messageString.matchAll(/<a?:(\w+):(\d+)>/gi);
+    const emojiUrls = [];
 
-	for (const emojiString of emojiStrings) {
-		// Fetch required info about the emoji
-		const emoji = getCustomEmojiById(emojiString[2]);
-		// Check emoji usability
-		if (
-			emoji.guildId != getGuildId() ||
-			emoji.animated
-		) {
-			// Remove emote from original msg
-			messageString = messageString.replace(emojiString[0], "");
-			// Add to emotes to send
-			emojiUrls.push(`[${emoji.name}](${emoji.url.split("?")[0]}?size=${size}&name=${emoji.name})`);
-		}
-	}
+    for (const emojiString of emojiStrings) {
+        // Fetch required info about the emoji
+        const emoji = getCustomEmojiById(emojiString[2]);
+        // Check emoji usability
+        if (
+            emoji.guildId != getGuildId() ||
+            emoji.animated
+        ) {
+            // Remove emote from original msg
+            messageString = messageString.replace(emojiString[0], "");
+            // Add to emotes to send
+            if (storage.hyperLink === true)
+                emojiUrls.push(`[${emoji.name}](${emoji.url.split("?")[0]}?size=${size}&name=${emoji.name})`);
+            else
+                emojiUrls.push(`${emoji.url.split("?")[0]}?size=${size}&name=${emoji.name}`);
+        }
+    }
 
-	return { 
+    return {
         newContent: messageString.trim(),
         extractedEmojis: emojiUrls,
     };
 }
 
 export default function modifyIfNeeded(msg: Message) {
-	if (!msg.content.match(hasEmotesRegex)) return;
+    if (!msg.content.match(hasEmotesRegex)) return;
 
-	// Find all emojis from the captured message string and return object with emojiURLS and content
-	const { newContent, extractedEmojis } = extractUnusableEmojis(msg.content, storage.emojiSize);
+    // Find all emojis from the captured message string and return object with emojiURLS and content
+    const { newContent, extractedEmojis } = extractUnusableEmojis(msg.content, storage.emojiSize);
 
-	msg.content = newContent;
+    msg.content = newContent;
 
-	if (extractedEmojis.length > 0) msg.content += "\n" + extractedEmojis.join("\n");
+    if (extractedEmojis.length > 0) msg.content += "\n" + extractedEmojis.join("\n");
 
-	// Set invalidEmojis to empty to prevent Discord yelling to you about you not having nitro
-	msg.invalidEmojis = [];
+    // Set invalidEmojis to empty to prevent Discord yelling to you about you not having nitro
+    msg.invalidEmojis = [];
 };

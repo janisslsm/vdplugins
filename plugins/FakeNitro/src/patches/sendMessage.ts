9872bb19c9c94938889fd008b3eae9b97086083e
Jānis <97699850+janisslsm@users.noreply.github.com>
2025-12-01 12:40:58 +0200

    fix(FakeNitro): fixed for newer versions

--- plugins/FakeNitro/src/patches/sendMessage.ts
+++ plugins/FakeNitro/src/patches/sendMessage.ts
@@ -1,11 +1,22 @@
-import { findByProps } from "@vendetta/metro";
+import { findByProps, findByStoreName } from "@vendetta/metro";
 import { before } from "@vendetta/patcher";
 import modifyIfNeeded from "../utils";
 
 const messageModule = findByProps("sendMessage", "receiveMessage");
 const uploadModule = findByProps("uploadLocalFiles");
+const { getCurrentUser } = findByStoreName("UserStore");
 
-export default [
-	before("sendMessage", messageModule, (args) => modifyIfNeeded(args[1])),
-	before("uploadLocalFiles", uploadModule, (args) => modifyIfNeeded(args[0].parsedMessage)),
+const patches = [
+	before("sendMessage", messageModule, (args) => {
+		if (getCurrentUser?.().premiumType === null) modifyIfNeeded(args[1]);
+	}),
 ];
+
+if (uploadModule?.uploadLocalFiles !== undefined) {
+	patches.push(
+		before("uploadLocalFiles", uploadModule, (args) =>
+			modifyIfNeeded(args[0].parsedMessage),
+		),
+	);
+}
+export default [...patches];

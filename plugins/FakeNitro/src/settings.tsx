1d14007210ba245379f27f65093cd6132468a01d
Jānis <janisslsm@janisslsm.id.lv>
2026-01-26 12:26:01 +0200

    feat(FakeNitro): use redesign components for settings

--- plugins/FakeNitro/src/settings.tsx
+++ plugins/FakeNitro/src/settings.tsx
@@ -2,61 +2,75 @@ import { ReactNative as RN } from "@vendetta/metro/common";
 import { Forms } from "@vendetta/ui/components";
 import { useProxy } from "@vendetta/storage";
 import { storage } from "@vendetta/plugin";
+import { findByProps } from "@vendetta/metro";
 
-const { FormSection, FormRadioRow, FormSwitchRow, FormDivider } = Forms;
+const {
+	TableRow,
+	TableSwitchRow,
+	TableRadioGroup,
+	TableRadioRow,
+	TableRowGroup,
+} = findByProps("TableRow");
+const { Stack } = findByProps("Stack");
 
 const sizeOptions = {
-    Tiny: 16,
-    Small: 32,
-    Medium: 48,
-    Large: 64,
-    Huge: 96,
-    Jumbo: 128,
-}
+	Tiny: 16,
+	Small: 32,
+	Medium: 48,
+	Large: 64,
+	Huge: 96,
+	Jumbo: 128,
+};
 
 const previewUri = "https://cdn.discordapp.com/emojis/926602689213767680.webp";
 
 export default () => {
-    useProxy(storage);
+	useProxy(storage);
 
-    return (
-        <RN.ScrollView style={{ flex: 1 }} contentContainerStyle={{ paddingBottom: 38 }}>
-            <FormSection title="Emoji Size" titleStyleType="no_border">
-                {Object.entries(sizeOptions).map(([name, size]) => <FormRadioRow
-                    label={name}
-                    subLabel={size}
-                    selected={storage.emojiSize === size}
-                    onPress={() => {
-                        storage.emojiSize = size;
-                    }}
-                />)}
-            </FormSection>
-            <FormSection title="Preview">
-                <RN.Image
-                    source={{ 
-                        uri: `${previewUri}?size=${storage.emojiSize}`,
-                        width: storage.emojiSize,
-                        height: storage.emojiSize 
-                    }}
-                />
-            </FormSection>
-            <FormSection title="User Profile" titleStyleType="no_border">
-                <FormSwitchRow
-                    label="Use hyperlinks when sending fake emojis"
-                    onValueChange={(v) => {
-                        storage.hyperLink = v;
-                    }}
-                    value={storage.hyperLink}
-                />
-                <FormDivider />
-                <FormSwitchRow
-                    label="Transform fake emojis into real ones"
-                    onValueChange={(v) => {
-                        storage.transformEmoji = v;
-                    }}
-                    value={storage.transformEmoji}
-                />
-            </FormSection>
-        </RN.ScrollView>
-    )
-}
+	return (
+		<Stack
+			style={{ paddingVertical: 24, paddingHorizontal: 12 }}
+			spacing={24}
+		>
+			<TableRadioGroup
+				title="Emoji size"
+				defaultValue={storage.emojiSize.toString()}
+				onChange={(v: string) => (storage.emojiSize = parseInt(v))}
+			>
+				{Object.entries(sizeOptions).map(([name, size]) => (
+					<TableRadioRow
+						label={name}
+						subLabel={size}
+						key={size.toString()}
+						value={size.toString()}
+					/>
+				))}
+			</TableRadioGroup>
+			<TableRowGroup title="Preview">
+				<RN.Image
+					source={{
+						uri: `${previewUri}?size=${storage.emojiSize}`,
+						width: storage.emojiSize,
+						height: storage.emojiSize,
+					}}
+				/>
+			</TableRowGroup>
+			<TableRowGroup title="User Profile" titleStyleType="no_border">
+				<TableSwitchRow
+					label="Use hyperlinks when sending fake emojis"
+					onValueChange={(v) => {
+						storage.hyperLink = v;
+					}}
+					value={storage.hyperLink}
+				/>
+				<TableSwitchRow
+					label="Transform fake emojis into real ones"
+					onValueChange={(v) => {
+						storage.transformEmoji = v;
+					}}
+					value={storage.transformEmoji}
+				/>
+			</TableRowGroup>
+		</Stack>
+	);
+};

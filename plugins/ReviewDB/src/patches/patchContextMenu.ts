1ac50bd2fb508a2f7c1784e88f9f34fe428d559e
Jānis <janisslsm@janisslsm.id.lv>
2026-01-29 11:18:54 +0200

    fix(ReviewDB): don't show context menu on anything other than servers hopefully

--- plugins/ReviewDB/src/patches/patchContextMenu.ts
+++ plugins/ReviewDB/src/patches/patchContextMenu.ts
@@ -6,14 +6,18 @@ import { ActionSheet } from "../components/ActionSheet";
 let ContextMenuPopout = findByProps("ContextMenuPopout");
 
 export default () =>
-	before("ContextMenuPopout", ContextMenuPopout, (args) => {
-		const userId = args[0]?.menu?.key;
-		if (userId !== undefined && args[0]?.menu?.items?.length === 3) {
-			args[0].menu.items.push({
-				label: "Reviews",
-				action: () => {
-					ActionSheet.open(ReviewActionSheet, { userId });
-				},
-			});
-		}
-	});
+    before("ContextMenuPopout", ContextMenuPopout, (args) => {
+        const userId = args[0]?.menu?.key;
+        if (
+            userId !== undefined &&
+            args[0]?.menu?.items?.length === 3 &&
+            userId?.length >= 17
+        ) {
+            args[0].menu.items.push({
+                label: "Reviews",
+                action: () => {
+                    ActionSheet.open(ReviewActionSheet, { userId });
+                },
+            });
+        }
+    });
--- plugins/ReviewDB/src/patches/patchServer.ts
+++ plugins/ReviewDB/src/patches/patchServer.ts
@@ -6,7 +6,7 @@ import ReviewCard from "../components/ReviewCard";
 let GuildActionSheetProgress = findByName("GuildActionSheetProgress", false);
 
 export default () =>
-	instead("default", GuildActionSheetProgress, (args, ret) => {
-		const guildId = args[0]?.guild?.id;
-		return React.createElement(ReviewCard, { userId: guildId });
-	});
+    instead("default", GuildActionSheetProgress, (args, ret) => {
+        const guildId = args[0]?.guild?.id;
+        return React.createElement(ReviewCard, { userId: guildId });
+    });

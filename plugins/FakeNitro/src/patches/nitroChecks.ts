d75660033c7ac1fc93bf4bfc4215d38d01131d2f
Jānis <janisslsm@janisslsm.id.lv>
2026-01-30 09:05:32 +0200

    fix(FakeNitro): ig filtering doesn't work that way perhaps another time

--- plugins/FakeNitro/src/patches/nitroChecks.ts
+++ plugins/FakeNitro/src/patches/nitroChecks.ts
@@ -9,17 +9,13 @@ export default [
 	instead("canUseEmojisEverywhere", nitroInfo, (args, orig) => {
 		if (getCurrentUser?.().premiumType !== null)
 			return orig.apply(this, args);
-		var stack_str = new Error().stack;
-		if (stack_str?.includes("at EmojiPicker")) return true;
 
-		return orig.apply(this, args);
+		return true;
 	}),
 	instead("canUseAnimatedEmojis", nitroInfo, (args, orig) => {
 		if (getCurrentUser?.().premiumType !== null)
 			return orig.apply(this, args);
-		var stack_str = new Error().stack;
-		if (stack_str?.includes("at EmojiPicker")) return true;
 
-		return orig.apply(this, args);
+		return true;
 	}),
 ];

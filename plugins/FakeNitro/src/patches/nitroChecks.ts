9cbc385aa59d786ff5e43e7b3a83fb7c7950d669
Jānis <janisslsm@janisslsm.id.lv>
2026-01-30 08:56:47 +0200

    feat(FakeNitro): don't allow reactions

--- plugins/FakeNitro/src/patches/nitroChecks.ts
+++ plugins/FakeNitro/src/patches/nitroChecks.ts
@@ -1,9 +1,25 @@
-import { findByProps } from "@vendetta/metro";
+import { findByProps, findByStoreName } from "@vendetta/metro";
 import { instead } from "@vendetta/patcher";
 
 const nitroInfo = findByProps("canUseEmojisEverywhere");
 
+const { getCurrentUser } = findByStoreName("UserStore");
+
 export default [
-	instead("canUseEmojisEverywhere", nitroInfo, () => true),
-	instead("canUseAnimatedEmojis", nitroInfo, () => true),
+	instead("canUseEmojisEverywhere", nitroInfo, (args, orig) => {
+		if (getCurrentUser?.().premiumType !== null)
+			return orig.apply(this, args);
+		var stack_str = new Error().stack;
+		if (stack_str?.includes("at EmojiPicker")) return true;
+
+		return orig.apply(this, args);
+	}),
+	instead("canUseAnimatedEmojis", nitroInfo, (args, orig) => {
+		if (getCurrentUser?.().premiumType !== null)
+			return orig.apply(this, args);
+		var stack_str = new Error().stack;
+		if (stack_str?.includes("at EmojiPicker")) return true;
+
+		return orig.apply(this, args);
+	}),
 ];
